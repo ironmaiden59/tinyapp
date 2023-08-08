918a3beb9ffda2b56726449ed13f8a1d68ba411f
ralph <ralphdcouto1987@gmail.com>
2023-08-07 22:33:18 -0400

    created ejs file and pasted the html skeleton

--- express_server.js
+++ express_server.js
@@ -17,8 +17,9 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
